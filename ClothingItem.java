8a295d2a8c08003634305a2cd1c15c46c428aa29
Ikramik <96791130+Ikramik@users.noreply.github.com>
2025-09-08 15:34:39 +0200

    Update ClothingItem.java

--- ClothingItem.java
+++ ClothingItem.java
@@ -1,18 +1,30 @@
 package com.example.eyec;
 
-
 public class ClothingItem {
     private String id;
     private String color;
-    private String type;
+    private String imagePath;
 
-    public ClothingItem(String id, String color, String type) {
+    public ClothingItem(String id, String color, String imagePath) {
         this.id = id;
         this.color = color;
-        this.type = type;
+        this.imagePath = imagePath;
     }
 
     public String getId() { return id; }
     public String getColor() { return color; }
-    public String getType() { return type; }
+    public String getImagePath() { return imagePath; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ClothingItem that = (ClothingItem) obj;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
